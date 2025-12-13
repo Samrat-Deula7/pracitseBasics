1d01d82160f826165199bfedf850817eefbd6cd2
Samrat-Deula7 <samratdeula461@gmail.com>
2025-12-13 08:59:05 +0545

    Practising my fundamentals

---
+++ s-product3.js
@@ -0,0 +1,22 @@
+var smallimg3=document.getElementsByClassName("small-img3");
+var info3=document.getElementById("info3");
+
+
+smallimg3[0].addEventListener("click",()=>{
+    info3.innerText="  1 Piece Crystal Wind Chime";
+})
+smallimg3[1].addEventListener("click",()=>{
+    info3.innerText="   Plain Tall Water Glass| Set of 6";
+})
+smallimg3[2].addEventListener("click",()=>{
+    info3.innerText=" Black Bead Heart Pendant Chain ";
+})
+smallimg3[3].addEventListener("click",()=>{
+    info3.innerText="  4pcs Soft Stretchable wide Strap scruncy  ";
+})
+smallimg3[4].addEventListener("click",()=>{
+    info3.innerText="  USB Handheld Mini Fan ";
+})
+smallimg3[5].addEventListener("click",()=>{
+    info3.innerText="     5pcs beautiful 5 inch Table Decor Flower Pot ";
+})
---
+++ script4.js
@@ -0,0 +1,22 @@
+var smallimg3=document.getElementsByClassName("small-img4");
+var price3=document.getElementById("price4");
+
+smallimg4[0].addEventListener("click",()=>{
+    price4.innerText="Rs1175";
+})
+smallimg4[1].addEventListener("click",()=>{
+    price4.innerText="Rs190";
+})
+
+smallimg4[2].addEventListener("click",()=>{
+    price4.innerText="Rs170";
+})
+smallimg4[3].addEventListener("click",()=>{
+    price4.innerText="Rs254";
+})
+smallimg4[4].addEventListener("click",()=>{
+    price4.innerText="Rs1687";
+})
+smallimg4[5].addEventListener("click",()=>{
+    price4.innerText="Rs560";
+})
